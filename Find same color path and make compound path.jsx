0eb00f3b8c88b22448733e2a164eaf35056af9b6
Abdul karim mia <97598943+abdul-karim-mia@users.noreply.github.com>
2022-01-13 20:01:26 +0530

    Update Find same color path and make compound path.jsx

--- Find same color path and make compound path.jsx
+++ Find same color path and make compound path.jsx
@@ -1,3 +1,25 @@
+/*
+ Find same color path and make compound path.jsx for Adobe Illustrator
+  Description: Find same color path and make compound path
+  Date: 13/01/2022
+  Author: Abdul karim mia
+  Mail: akmia51@gmail.com
+  Based on script by Abdul karim mia (http://www.abdulkarimmia.com/)
+  Installation: https://github.com/creold/illustrator-scripts#how-to-run-scripts
+  Release notes:
+  0.1 Initial version
+  Donate (optional):
+  If you find this script helpful, you can buy me a coffee
+
+  - via PayPal https://paypal.me/akmia51
+  NOTICE:
+  Tested with Adobe Illustrator CC 2018-2021 (Mac, Win).
+  This script is provided "as is" without warranty of any kind.
+  Free to use, not for sale
+  Released under the MIT license
+  http://opensource.org/licenses/mit-license.php
+  Check other author's scripts: https://github.com/abdul-karim-mia
+*/
 
 @target illustrator
 var lays=activeDocument.layers;
